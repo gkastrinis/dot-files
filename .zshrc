9aea711afb077f18c1ed125c6734683c5615df51
George Kastrinis <george.kast@gmail.com>
2025-10-03 16:50:26 +0300

    updates

--- .zshrc
+++ .zshrc
@@ -29,41 +29,42 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export TERM=xterm-256color
 
-alias ls='ls --color=auto'
-alias ll='ls -l'
+#alias ls='ls --color=auto'
+#alias ll='ls -l'
+alias ls='lsd'
+alias ll='lsd -l'
+alias lt='lsd -l --tree'
 alias cp='cp -i -r'
 alias mv='mv -i'
 alias rm='rm -rf'
 #alias scp='scp -r'
 #alias mirror='rsync -avz --delete'
 #alias cat=bat
-alias ag='ag --noheading'
-alias agg='cls ; ag'
-alias grepr='grep --color=auto -I -rn'
+#alias ag='ag --noheading -s'
+#alias agg='cls ; ag'
+#alias grepr='grep --color=auto -I -rn'
 #alias pgrep='pgrep -l -f -u $(whoami)'
 #alias du='du -csh'
 #alias tree='tree -C'
 alias uprc='source $HOME/.zshrc'
-alias vim='vim -p'
+#alias vim='vim -p'
 #alias x='xdg-open'
 #alias fixgpg='pkill gpg-agent ; gpg-agent --daemon'
 alias cls='yes "" | head -$(tput lines)'
 alias gcc='gcc -ansi -pedantic -Wall -Wextra -W -Wshadow -std=c11'
 alias g++='g++ -std=c++0x -pedantic -Wall -W -Wshadow'
 alias python=python3
-alias tic80='tic80 --skip --fs=.'
-
-#fixperms() {
-#	find $1 -type d -exec chmod 700 {} \;
-#	find $1 -type f -exec chmod 600 {} \;
-#}
+#alias tic80='tic80 --skip --fs=.'
+alias jl='julia -t 8'
+alias pytest='pytest -W ignore::DeprecationWarning -s'
+alias tea='$HOME/Work/relationalai-python/scripts/test/test_ea.sh'
+alias vin='nvim'
 
 #case $(hostname) in
 #	'almanac' )
 		#source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 		#export CLASSPATH=".:/usr/local/lib/antlr-4.9.3-complete.jar:$CLASSPATH"
 		#alias antlr4='java -jar /usr/local/lib/antlr-4.9.3-complete.jar'
-		#alias julia='/usr/local/bin/julia -t 8'
 #		;;
 #	'Anorak' )
 		#alias up='sudo apt-get update ; sudo apt-get -y upgrade ; sudo apt-get -y dist-upgrade ; sudo apt-get autoclean ; sudo apt-get autoremove'
@@ -71,27 +72,12 @@ alias tic80='tic80 --skip --fs=.'
 #		;;
 #esac
 
-# Find and set branch name var if in git repository.
-#function git_branch_name()
-#{
-#  branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
-#  if [[ $branch == "" ]];
-#  then
-#    :
-#  else
-#    echo $branch
-#  fi
-#}
-# Enable substitution in the prompt.
-#setopt prompt_subst
-# Config for prompt. PS1 synonym.
-#prompt='%{$fg[magenta]%}$(git_branch_name)%{$reset_color%}/%1~ 🍭 '
-
 eval "$(shadowenv init zsh)"
 export PATH=/usr/local/sbin:/usr/local/bin:$HOME/dot-files:$PATH:.
-#export PATH=$JAVA_HOME/bin:$PATH:.
-#export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
-export PATH="/Applications/tic80.app/Contents/MacOS/:$PATH"
+export PATH=$HOME/nvim-macos-x86_64-nightly/bin:$PATH
+#export PATH="/Applications/tic80.app/Contents/MacOS/:$PATH"
+export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+export PATH="$HOME/.julia/artifacts/d10d213b97cf181a9138de58f6e9d01156d4b3bc/bin/:$PATH"
 
 # >>> juliaup initialize >>>
 
@@ -101,5 +87,7 @@ path=('/Users/georgekastrinis/.juliaup/bin' $path)
 export PATH
 
 # <<< juliaup initialize <<<
+
+source $HOME/Work/pyautoenv/pyautoenv.plugin.zsh
 
 eval "$(oh-my-posh init zsh --config ~/gkastrinis.info.json)"
--- startup.jl
+++ startup.jl
@@ -1,9 +1,8 @@
 import Pkg
-
-#Pkg.activate()
-#Pkg.add(name="Revise", version="3.4")
-#Pkg.activate(".")
 using Revise
+
+#Pkg.instantiate(); Pkg.build("RAICode")
+#using RAICode.Compiler.Front: debug_set_query_layering!; debug_set_query_layering!(; enabled=true)
 
 atreplinit() do repl
     @eval using OhMyREPL
@@ -14,53 +13,127 @@ atreplinit() do repl
         OhMyREPL.Prompt.insert_keybindings()
         OhMyREPL.enable_fzf(true)
     end
-    #repl.options.confirm_exit = true
+    # repl.options.confirm_exit = true
 end
 
-#hey() = run(`say 'hey dude'`)
-hey() = run(`say 'all your base are belong to us'`)
-
+#=
 using RAICode
-using RelationalAI
-using RAICode.Test
-
-#ENV["DASSERT_LEVEL"] = 2
-#ENV["DATALOG_JL_DEBUG"] = true
-ENV["RAI_TRACEVIEW"] = 42
-ENV["RAI_PRINT_CONSTANTS_HASHED"] = 0
-
-conn = nothing
-## === v1 === ###
-function db(;dbname=:george, port=12123)
-    global conn
-    RAICode.Server.start!(RAICode.Server.RAIServer(RAICode.Server.Configuration(port=port)))
-    #conn = RelationalAI.LocalConnection(;dbname=dbname, port=port)
-    conn = RAICode.API.EmbeddedConnection(dbname)
-    create_database(conn; overwrite=true)
-    hey()
-    return conn
+using RAICode.API
+using RAICode.Server
+using RelationalAI: LocalConnection
+loc(dbname) = LocalConnection(;dbname=dbname)
+emb(dbname) = API.EmbeddedConnection(dbname)
+ser = nothing
+function serve()
+    global ser
+    ser = Server.start!(Server.RAIServer(Server.Configuration()))
+    return nothing
 end
+dbname = nothing
+function db()
+    global dbname
+    dbname = API.create_database(;prefix="george")
+    run(`say "work done"`)
+    return dbname
+end
+=#
 
-q(args...; keyargs...) = query(conn, args...; keyargs...)
-f(path; keyargs...) = q(read(path, String); keyargs...)
+#=
+using RAI
+ctx = Context(load_config(; profile="local"))
+v2(q::String; keyargs...) = v2(q, string(dbname); keyargs...)
+v2(q::String, dbname::String; keyargs...) = exec(ctx, dbname, "no-matter", q; keyargs...)
+p2(q::String) = show_result(v2(q))
+f2(path; keyargs...) = v2(read(path, String); keyargs...)
+=#
 
-## === v2 === ##
-#ctx = nothing
-#function db2(;dbname=:george)
-#    global conn, ctx
-#    RAICode.Server.start!(RAICode.Server.RAIServer(RAICode.Server.Configuration(port=port)))
-#    conn = RAICode.API.EmbeddedConnection(dbname)
-#    create_database(conn; overwrite=true)
-#    ctx = Context(load_config(; profile="local"))
-#    return conn
-#end
+#run(`say "Work work"`)
+#@info "✨✨✨ Work! Work! ✨✨✨"
 
-#ex(q::String; dbname=:george) = exec(ctx, dbname, " no-matter", q)
-## ================== ##
+if isinteractive()
+    @eval import BasicAutoloads
+    @eval BasicAutoloads.register_autoloads([
+        ["@benchmark", "@btime"] => :(using BenchmarkTools),
+        ["@infiltrate"] => :(using Infiltrator),
+        ["@test_rel", "runtests"] => :(using RAICode.Test, ReTestItems),
+        ["@t"] => quote
+            using RAICode.Test
+            using ReTestItems
+            macro t(s); return :( runtests($s) ); end
+            macro t(s, name); return :( runtests($s; name=$name) ); end
+        end,
+        ["@mig", "@migstr", "corerel_migrate_file", "corerel_migrate_dir", "__test_migrate"] => quote
+            using RAI_Primitives.CoreRelMigrator: corerel_migrate, corerel_migrate_file, corerel_migrate_dir, __test_migrate
+            macro mig(p)
+                endswith(p, "/abc.rel") && return :( __test_migrate($p) )
+                isdir(p) && return :( corerel_migrate_dir($p) )
+                return :( corerel_migrate_file($p) )
+            end
+            macro migstr(str)
+                return corerel_migrate(str)
+            end
+        end,
+        ["set_dassert_level"] => :(using RelationalAIBase),
+        ["v1", "f1", "db"] => quote
+            using RAICode
+            using RAICode.API
+            db = API.create_database(;prefix="george")
+            run(`say "work done"`)
+            v1(args...; keyargs...) = query(db, args...; keyargs...)
+            f1(path; keyargs...) = v1(read(path, String); keyargs...)
+        end,
+        ["pp"] => quote
+            using RelationalAITypes
+            function pp(dict::RelationalAITypes.RelDict)
+                for (k, v) in dict
+                    printstyled("\n", k, "\n"; color = :blue)
+                    pp(v)
+                end
+            end
 
-using ReTestItems
-T = runtests
-using XUnit: runtests as xunittests
-X(s) = xunittests([s])
+            function pp(vs::Vector)
+                for v in vs
+                    pp(v)
+                    println()
+                end
+            end
 
-hey()
+            function pp(t::Tuple)
+                for v in t
+                    pp(v)
+                    print("\t")
+                end
+            end
+
+            function pp(x)
+                print(x)
+            end
+        end,
+        ["run_lqp"] => quote
+            using LogicalQueryProtocol: Transaction, is_read_only
+            using ProtoBuf
+            using RAICode: Database
+
+            function run_lqp(db, filename::String)
+                # Read the file
+                data = IOBuffer(read(filename))
+
+                # Decode the transaction
+                decoded = ProtoBuf.ProtoDecoder(data)
+                txn = ProtoBuf.decode(decoded, Transaction)
+                # println("decoded transaction: ", txn)
+
+                Database.test_transaction(db; isreadonly=is_read_only(txn)) do txn_handle
+                    rt = Database.salsa_runtime(txn_handle)
+
+                    # Run the query
+                    result = RAICode.LQPDriver.execute!(rt, txn)
+
+                    # Show result
+                    @show result
+                    return result
+                end
+            end
+        end,
+    ])
+end
